702ea8f3d26771e49512f8f9ae0b0422e7800568
wildanmujjahid29 <wildan.10122292@mahasiswa.unikom.ac.id>
2025-08-24 20:48:43 +0700

    Update main.py
    Co-Authored-By: Wildan Mujjahid Robbani <136696980+wildanmujjahid29@users.noreply.github.com>

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 from api.document_router import router as document_router
 from api.service_router import router as service_router
 from api.prompt_router import router as prompt_router
+from api.retrieval_router import router as retrieval_router
+from api.rag_router import router as rag_router
 
 app = FastAPI()
 
@@ -23,7 +25,9 @@ def root():
         "app": "Backend Chatbot RAG",
         "tips": "Gunakan endpoint /docs untuk eksplorasi API ini!"
     }
-
+    
+app.include_router(rag_router, tags=["RAG"])
 app.include_router(document_router, tags=["Documents"])
+app.include_router(retrieval_router, tags=["Retrieval"])
 app.include_router(service_router, tags=["Services"])
 app.include_router(prompt_router, tags=["Prompts"])
