aca682463af2fa648b29d662db8a9ebc00daf523
wildanmujjahid29 <wildan.10122292@mahasiswa.unikom.ac.id>
2025-09-04 18:50:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
 
-from api.document_router import router as document_router
-from api.service_router import router as service_router
-from api.prompt_router import router as prompt_router
-from api.retrieval_router import router as retrieval_router
-from api.rag_router import router as rag_router
+# from fastapi.middleware.cors import CORSMiddleware
+from api.chat_router import router as chat_router
 from api.dashboard_router import router as dashboard_router
+from api.document_router import router as document_router
+from api.prompt_router import router as prompt_router
+from api.rag_router import router as rag_router
+from api.restricted_topics_router import router as restricted_topics_router
+from api.retrieval_router import router as retrieval_router
+from api.service_router import router as service_router
 
 app = FastAPI()
 
@@ -25,11 +27,13 @@ def root():
         "message": "Haloo! Semoga harimu menyenangkan",
         "app": "Backend Chatbot RAG",
         "tips": "Gunakan endpoint /docs untuk eksplorasi API ini!"
-    }
+    } 
     
-app.include_router(rag_router, tags=["RAG"])
+app.include_router(chat_router, tags=["Chat"])
+app.include_router(rag_router, tags=["RAG"], include_in_schema=False)
 app.include_router(dashboard_router, tags=["Dashboard"])
 app.include_router(document_router, tags=["Documents"])
-app.include_router(retrieval_router, tags=["Retrieval"])
+app.include_router(retrieval_router, tags=["Retrieval"], include_in_schema=False)
 app.include_router(service_router, tags=["Services"])
 app.include_router(prompt_router, tags=["Prompts"])
+app.include_router(restricted_topics_router, tags=["Restricted Topics"])
